a0e4c7aad5888da228fc69c7e56ecc8259798e96
Yaroslav Khakhuda <hahyda14@gmail.com>
2022-11-13 22:20:28 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const refs = {
+	changeColorBtn: document.querySelector('.change-color'),
+	colorSpan: document.querySelector('.color'),
+};
+
+refs.changeColorBtn.addEventListener('click', onClickChangeColor);
+
+function onClickChangeColor() {
+	const color = getRandomHexColor();
+	document.body.style.backgroundColor = color;
+	refs.colorSpan.textContent = color;
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }

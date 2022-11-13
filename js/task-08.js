ff216a291402c9f5f302f4c5d92c639f43d6e5bb
Yaroslav Khakhuda <hahyda14@gmail.com>
2022-11-13 22:04:09 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmitAuth);
+
+function onSubmitAuth(event) {
+	event.preventDefault();
+
+	if (
+		event.currentTarget.email.value === '' ||
+		event.currentTarget.password.value === ''
+	) {
+		return alert('Всі поля повинні бути заповнені!');
+	} else {
+		const formData = new FormData(event.currentTarget);
+		const data = {};
+		formData.forEach((value, name) => {
+			data[name] = value;
+		});
+
+		console.log(data);
+		event.currentTarget.reset();
+	}
+}
